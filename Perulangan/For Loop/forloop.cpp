e452d43b0ddeecfa9535c366b5dfe147a56c57da
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-05-20 00:24:30 +0700

    Update forloop.cpp

--- Perulangan/For Loop/forloop.cpp
+++ Perulangan/For Loop/forloop.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Masukan jumlah perulangan ";
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cout<<"Perulangan Ke-" << i << endl;
+	}
+	return 0;
+}
